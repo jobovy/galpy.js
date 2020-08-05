2114e10beecc150f7393e23c1d4accc68bdff517
Jo Bovy <bovy@astro.utoronto.ca>
2020-08-05 14:28:51 -0400

    Orbit test of initial energy and of error message when plotting in node

--- test/test_orbit.js
+++ test/test_orbit.js
@@ -28,14 +28,32 @@ pots.forEach(pot => {
 	let o= new orbit.Orbit([1.,0.1,1.1,0.1,-0.2,0.3]);
 	pot.normalize(1.);
 	o.integrate(times,pot,rtol=rtol,atol=atol);
+	let Energy= o.E(true);
 	let tol= (`${pot.constructor.name}` in special_tol)
 	    ? special_tol[`${pot.constructor.name}`]
 	    : default_tol;
 	it('Energy conserved', function () {
-	    let Energy= o.E(true);
 	    assert.isBelow(Energy.mult(1./Energy.mean()).std(),
 			   tol,
 			   `Energy is not conserved during orbit integration in ${pot.constructor.name}`);
 	});
+	it('Energy close to initial', function () {
+	    assert.approximately(o.E(),Energy.mean(),
+			   tol,
+			   `Energy does not stay close to initial energy during orbit integration in ${pot.constructor.name}`);
+	});
+    });
+});
+
+/*
+ * Test that orbit plotting doesn't work in Node.js
+ */
+describe('Orbit plotting in Node.js', function () {
+    let o= new orbit.Orbit([1.,0.1,1.1,0.1,-0.2,0.3]);
+    let pot= new potential.IsochronePotential().normalize(1.);
+    o.integrate(times,pot,rtol=rtol,atol=atol);
+    it('throws error because it should not work', function () {
+	let expected_error= 'Plotting a galpy.js orbit only works in the browser, not in node';
+	expect(o.plot).to.throw(expected_error);
     });
 });
