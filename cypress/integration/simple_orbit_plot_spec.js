611000fba08c9991cc88330ae131794484798fcb
Jo Bovy <bovy@astro.utoronto.ca>
2022-07-21 10:06:28 -0400

    Upgrade to Cypress 10

--- cypress/integration/simple_orbit_plot_spec.js
+++
@@ -1,8 +0,0 @@
-describe('The simple_orbit_plot page', () => {
-  it('successfully loads', () => {
-      cy.visit('/examples/simple_orbit_plot.html')
-  })
-  it('and makes the plot', () => {
-      cy.get('#galpy-orbit-graph').find('.plot-container')
-  })
-})
